26eb434bcbd2a5e67d36492fde7236a8e3dc6678
ProtekDev <chaosofdeath06@gmail.com>
2023-04-28 09:43:31 +0200

    add docstring to functions

--- patch_binary.py
+++ patch_binary.py
@@ -1,6 +1,16 @@
 import os
 
 def get_bytes(content):
+    """
+    Get bytes if input was for example a string.
+    
+    Args:
+        content (string or bytes): can be a any of those
+        
+    Returns:
+        bytes
+    """
+        
     if type(content) == bytes:
         return content
     else:
@@ -10,13 +20,40 @@ def get_bytes(content):
         byte_array = escaped_string.encode('utf-8').decode('unicode_escape').encode('latin-1')
         return bytes(byte_array)
     
+    
 def patch_at_offset(filepath, offset, patch):
+    """
+    Patch bytes at specific offset.
+    
+    Args:
+        filepath (string): the full path to the file you want to patch (.exe).
+        offset (int or hexstring): the offset in your file
+        patch (string or bytes): the new bytes you want to use.
+        
+    Returns:
+        nothing.
+    """
+
     patch = get_bytes(patch) # set to bytes if not already
     with open(filepath, 'r+b') as fh:
         fh.seek(offset)
         fh.write(patch)
 
+
 def patch_scan(filepath, find, replace):
+    # docstring
+    """
+    Patch bytes from 'find' with 'replace'.
+    
+    Args:
+        filepath (string): the full path to the file you want to patch (.exe).
+        find (string or bytes): the bytes you want to find and patch
+        replace (string or bytes): the new bytes you want to use
+        
+    Returns:
+        nothing.
+    """
+
     find = get_bytes(find) # set to bytes if not already
     replace = get_bytes(replace) # set to bytes if not already
     if len(find) != len(replace):
